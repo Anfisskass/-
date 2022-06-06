157a5021860904276ba5ecf655c51ac004f50292
anfisskass <nasyazh@mail.ru>
2022-06-06 11:42:41 +0300

    First seminal completed

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num;
+
+Console.Write ("Input integer number: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+int current = num * (-1);
+
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current++; //тоже самое что current = current + 1
+}
