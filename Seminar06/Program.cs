09a3331a7d3c449c70c5fa0fe230c2ca863a5dd7
anfisskass <nasyazh@mail.ru>
2022-07-07 00:06:04 +0300

    Lesson 6

---
+++ Seminar06/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] ReserseArray(int[] array)
+{
+    int temp;
+    int j = array.Length - 1, i = 0;
+    
+    while(i < j)
+    {
+        temp = array[i];
+        array [i] = array [j];
+        array [j] = temp;
+
+        i++;
+        j--;
+    }
+    return array;
+}
+
+int[] newArray = {1, 2, 3, 4, 5};
+
+newArray = ReserseArray(newArray);
+
+for(int i = 0; i < newArray.Length; i++)
+{
+    Console.Write(newArray[i] + " ");
+}
+
+
+Console.WriteLine ("Input numbers: ");
+int M = Convert.ToInt32(Console.ReadLine());
