22c2c0c1039ce0c53e8839ff88b828b959240edf
anfisskass <nasyazh@mail.ru>
2022-07-20 18:21:56 +0300

    changing in seminar10

--- Seminar10/Program.cs
+++ Seminar10/Program.cs
@@ -1,87 +1,67 @@
-﻿//Метод который на вход берет массив, на выход кол-во строк у которых слова больше 5 символов
-//string[] names = {"Ivan", "Anna", "Max", "Denis"};
+﻿// Задача 1: Задайте массив строк. Напишите программу, считает кол-во слов в массиве, начинающихся на гласную букву.
+// Пример: { "qwe", "wer", "ert", "rty", "tyu"} -> 1
+
+
+int CountWordsVowel(string[] array, char a, char b, char c, char d, char m, char n)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i][0] == a || array[i][0] == b || array[i][0] == c || array[i][0] == d || array[i][0] == m || array[i][0] == n )
+            count ++;
+    }
+    return count;
+
+}
+char a = 'A';
+char b = 'E';
+char c = 'I';
+char d = 'O';
+char m = 'U';
+char n = 'Y';
+
+string [] array1 = {"Anna", "Jane", "Jennifer", "Gabriele", "Anastasiya", "Diana", "Emilie"};
+Console.WriteLine(CountWordsVowel(array1, a, b, c, d, m, n));
+
 
 /*
+// Задача 2: Задайте массив строк. Напишите программу, которая генерирует новый массив, объединяя элементы исходного массива попарно.
+// Пример: { "qwe", "wer", "ert", "rty", "tyu", "yui"} -> { "qwewer", "ertrty", "tyuyui"}
+
 string[] CreateStringArray(int size)
 {
     string[] words = new string[size];
-
-    for(int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
     {
-        Console.Write($"Input {i + 1} word");
+        Console.Write($"Input {i + 1} word: ");
         words[i] = Console.ReadLine();
     }
     return words;
 }
-
 void ShowArray(string[] array)
 {
     for(int i = 0; i < array.Length; i++)
-        Console.Write(array[i] + " ");
-    
-    Console.WriteLine();
+    {
+        Console.WriteLine(array[i] + " ");
+            Console.WriteLine();
+    }
 }
-
-int NumberOfLongWords(string[] array)
+string[] GenerateNewArray(string[] array)
 {
-    int count = 0;
-
-    for(int i = 0; i < array.Length; i++)
-    if(array[i].Length >= 5)
-    count++;
-    return count; 
+    int size = array.Length/2;
+    if(array.Length % 2 != 0) size += 1;
+    string[] result = new string[size];
+    int j = 0;
+    for(int i = 0; i < array.Length/2; i++, j+=2)
+    {
+        result[i] = array[j] + array[j + 1]; 
+    } 
+    if(array.Length % 2 != 0) 
+        result[size-1] = array[array.Length-1];
+    return result;
 }
-
-Console.Write("Input number of names: ");
+Console.Write("Input number of elements: ");
 int size = Convert.ToInt32(Console.ReadLine());
-
-string[] names = CreateStringArray();
-
-Console.WriteLine("Number of long word is  " + NumberOfLongWords);
+string[] words = CreateStringArray(size);
+ShowArray(GenerateNewArray(words));
 */
-
-
-//Задача 2. Написать программу, которая принимает на вход два массива строк и возвращает
-// массив из попарно объединенных исходных элементов.
-
-string[] PairArray(string[] array1, string[] array2)
-{
-    string[] minArray = array1;
-    string[] maxArray = array2;
-
-    if(array2.Length < array2.Length) 
-    {
-         minArray = array2;
-         maxArray = array1;
-    }
-
-     for(int i = 0; i < maxArray.Length; i++)
-    {
-        if(i < minArray.Length)
-        newArray[i] +=  (" " + minArray[i]); //пробел
-    }
-    return maxArray;
-}
-
-string[] words1 = CreateStringArray(3);
-string[] words2 = CreateStringArray(5);
-
-string[] result = PairArray(words1, words2);
-ShowArray(result);
-
-
-//Задача 3. Написать программу, которая считает кол-во свло начинающихся на букву Y or W.
-
-int IfYW (string[] array)
-{
-    int count = 0;
-    for(int i = 0; i < array.Length; i++)
-    {
-        if(array[i][0] == 'y' || array[i][0] == 'w') count++;
-        
-        return count;
-    }
-}
-
-string[] words = CreateStringArray(5);
-Console.WriteLine(IfYW(words));
